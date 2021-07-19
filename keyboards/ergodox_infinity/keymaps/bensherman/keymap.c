9ed72a16503861e5b3fe46aefa6f3f7721cccb76
Ben Sherman <3365+bensherman@users.noreply.github.com>
2021-07-19 16:01:16 -0700

    remove default cruft

--- keyboards/ergodox_infinity/keymaps/bensherman/keymap.c
+++ keyboards/ergodox_infinity/keymaps/bensherman/keymap.c
@@ -6,13 +6,6 @@
 #define KC_MAC_COPY LGUI(KC_C)
 #define KC_MAC_PASTE LGUI(KC_V)
 
-enum custom_layers {
-    BASE,   // default layer
-    SYMB,   // symbols
-    MDIA,   // media keys
-};
-
-
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   VRSN,
@@ -22,8 +15,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ergodox_pretty(
     LT(3,KC_ESCAPE),KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_MINUS,                                       KC_EQUAL,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLASH,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           LALT(LGUI(KC_LCTRL)),                                RALT(RGUI(KC_RCTRL)),KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_QUOTE,
-    LCTL_T(KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),KC_ENTER,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           LALT(LGUI(KC_LCTRL)),                           RALT(RGUI(KC_RCTRL)),KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_QUOTE,
+  LCTL_T(KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),KC_ENTER,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_ESCAPE,                                      KC_LALT,        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         LT(1,KC_SLASH), KC_RSHIFT,
     LT(2,KC_GRAVE), KC_LALT,        KC_LGUI,        KC_LBRACKET,    LGUI_T(KC_LGUI),                                                                                                KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       TT(1),
                                                                                                     KC_DELETE,      KC_MAC_COPY,    KC_MAC_PASTE,   RGUI(KC_SPACE),
